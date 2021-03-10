298103a084c6da2c4e7207689fdf141a9218fc10
DaisukeKawamura <da1i9su9ke7.0715@icloud.com>
2021-03-10 22:59:59 +0900

    third-commit

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
 'use strict';
 
-const result = document.getElementById("result");
+const showFizzbuzzResult = document.getElementById('show-fizzbuzz-result');
+const executeFizzbuzzButton = document.getElementById('execute-fizzbuzz-button');
+
+console.log(executeFizzbuzzButton);
 
 // クリック時の挙動
-const on_click = () => {
-    (check_number(document.getElementById("fizznum").value) && check_number(document.getElementById("buzznum").value)) ? judge_fizzbuzz() : output_error_message();
-};
-
+executeFizzbuzzButton.addEventListener('click', () => {
+    const fizzValue = escapeHtml(document.getElementById('fizz-value').value);
+    const buzzValue = escapeHtml(document.getElementById('buzz-value').value);
+    (judgeWhetherInteger(fizzValue)) && judgeWhetherInteger(escapeHtml(buzzValue)) ? judgeFizzbuzz() : outputErrorMessage();
+});
 
 
 // 整数値か判定する関数
-const check_number = (value) => {
+const judgeWhetherInteger = (value) => {
     var regex = new RegExp(/^[0-9]+$/);
     return regex.test(value);
 }
 
 
 // 入力値を元にFizzBuzzを判定する関数
-const judge_fizzbuzz = () => {
-    const fizznum = document.getElementById("fizznum").value;
-    const buzznum = document.getElementById("buzznum").value;
-    result.innerHTML = "";
+const judgeFizzbuzz = () => {
+    const fizzValue = escapeHtml(document.getElementById('fizz-value').value);
+    const buzzValue = escapeHtml(document.getElementById('buzz-value').value);
+    showFizzbuzzResult.innerHTML = '';
     
-    for(let counter = 1; counter < 100; counter++){
-        if(counter % fizznum == 0 && counter % buzznum == 0){
-            result.innerHTML += "FizzBuzz" +  "  " + counter + "<br>";
-        }else if(counter % fizznum == 0){
-            result.innerHTML += "Fizz" + "  " + counter + "<br>";
-        }else if(counter % buzznum == 0){
-            result.innerHTML += "Buzz" + "  " + counter + "<br>";
-        }
+    for(let counter = 1; counter < 100; counter++) {
+        if(counter % fizzValue == 0 && counter % buzzValue == 0) {
+            showFizzbuzzResult.innerHTML += 'FizzBuzz' +  '  ' + counter + '<br>';
+        }else if(counter % fizzValue == 0) {
+            showFizzbuzzResult.innerHTML += 'Fizz' + '  ' + counter + '<br>';
+        }else if(counter % buzzValue == 0) {
+            showFizzbuzzResult.innerHTML += 'Buzz' + '  ' + counter + '<br>';
+        };
 
     };
 };
 
+// JSで動的動作を行う際のXSS対策の具体的な方法（エスケープ処理）
+const escapeHtml = (str) => {
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&#39;');
+    return str;
+  }
+
 
 // エラーメッセージを表示する関数
-const output_error_message = () => {
-    result.innerHTML = "整数値を入力してください。"
+const outputErrorMessage = () => {
+    showFizzbuzzResult.innerHTML = '整数値を入力してください。'
 };
